43d5ea4df1032e426b3db6ea76b765f8242f2dfd
Jeremiah-Williams <jae4lani17@gmail.com>
2024-06-23 21:01:11 -0400

    regexhw.py

---
+++ regexhw.py
@@ -0,0 +1,21 @@
+# 1. Python Regular Expressions Mastery
+
+# Task 1: Name Verification
+
+# Problem Statement: 
+#  Write a function that takes in a list of names,
+#  and verifies that the names are valid names using a regex pattern 
+# and match(), and prints the name if it is valid, 
+# "Invalid name" if the name is not.
+
+# Use the following list as an argument to test your function.
+
+
+
+# Code Example:
+import re
+names = ["Abraham Lincoln", "Andrew P Garfield", "peter pan", "Connor Milliken", "Jordan Alexander Williams", "Madonna", "programming is cool"]
+
+name_list = names
+re.findall(r"A.z", names)
+print(names)
